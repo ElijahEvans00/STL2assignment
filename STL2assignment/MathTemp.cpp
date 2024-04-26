39705e5cd1f999dba49c783550ad36338c95fb9e
Elijah Evans <elijah.evans1@sinclair.edu>
2024-04-26 11:35:24 -0400

    Added the definitions for the template in MathTemp.cpp. Created the special template definition for handling the rounding of ints.

--- STL2assignment/MathTemp.cpp
+++ STL2assignment/MathTemp.cpp
@@ -2,3 +2,18 @@
 // CIS 1202 201
 // April 26, 2024
 #include "MathTemp.h"
+
+template <typename T>
+T half(T value) {
+    return value / 2.0;
+}
+
+// Template for handling and rounding int variables
+template <>
+int half<int>(int value) {
+    return (round(value / 2.0));
+}
+
+template float half(float);
+template double half(double);
+template int half(int);
